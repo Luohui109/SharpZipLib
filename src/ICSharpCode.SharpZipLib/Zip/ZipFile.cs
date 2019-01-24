14865a7a28aac78b5502d13c82e701f79935fc58
Richard Webb <richard.webb@boldonjames.com>
2019-01-24 22:30:31 +0000

    Wire up BZip2 compression support for Zip files.

--- src/ICSharpCode.SharpZipLib/Zip/ZipFile.cs
+++ src/ICSharpCode.SharpZipLib/Zip/ZipFile.cs
@@ -883,6 +883,10 @@ namespace ICSharpCode.SharpZipLib.Zip
 					result = new InflaterInputStream(result, new Inflater(true));
 					break;
 
+				case CompressionMethod.BZip2:
+					result = new BZip2.BZip2InputStream(result);
+					break;
+
 				default:
 					throw new ZipException("Unsupported compression method " + method);
 			}
@@ -1899,7 +1903,7 @@ namespace ICSharpCode.SharpZipLib.Zip
 		/// <param name="compressionMethod">The compression method for the new entry.</param>
 		private void CheckSupportedCompressionMethod(CompressionMethod compressionMethod)
 		{
-			if (compressionMethod != CompressionMethod.Deflated && compressionMethod != CompressionMethod.Stored)
+			if (compressionMethod != CompressionMethod.Deflated && compressionMethod != CompressionMethod.Stored && compressionMethod != CompressionMethod.BZip2)
 			{
 				throw new NotImplementedException("Compression method not supported");
 			}
@@ -2631,6 +2635,14 @@ namespace ICSharpCode.SharpZipLib.Zip
 						IsStreamOwner = false
 					};
 					result = dos;
+					break;
+
+				case CompressionMethod.BZip2:
+					var bzos = new BZip2.BZip2OutputStream(result)
+					{
+						IsStreamOwner = false
+					};
+					result = bzos;
 					break;
 
 				default:
